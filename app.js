9f7934674a348ba8570fb0042e580d514ffb30f1
tillzmh <tillzmh@users.noreply.github.com>
2022-07-20 19:47:27 -0400

    variables added and DOM settings created

---
+++ app.js
@@ -0,0 +1,11 @@
+//variaubles
+const apiKey = "e4ce42a714cb9c3a4f80a484e0b90750";
+const daysForecast = 5;
+const storageName = "cities";
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+const textInput = document.querySelector("input");
+const main = document.querySelector("main");
+const nav = document.querySelector("nav");
+
